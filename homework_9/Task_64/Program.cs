8d2dd683aa641530f290f8ba998a7b594293688b
name <you@example.com>
2023-03-02 15:02:42 +0300

    hw9

---
+++ homework_9/Task_64/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("Задача 64: Задайте значение N. Напишите программу,"
+                 +"которая выведет все натуральные числа в промежутке от N до 1."
+                 +"Выполнить с помощью рекурсии.");
+int number = InputNumbers("Введите число N: ");
+string result = ShowNum(number);
+int index = result.LastIndexOf(',');
+result = result.Remove(index, 1);
+Console.WriteLine($"N = {number} -> {result}");
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+string ShowNum(int number, int start = 0)
+{
+        if (number > start)
+    {
+        return $"{number}, {ShowNum(number-1, start)}";
+    }
+   return string.Empty;
+
+}
+

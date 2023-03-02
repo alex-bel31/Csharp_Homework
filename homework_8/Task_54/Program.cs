db4d85ce1b4eb40d14f9c499673838abe8ffb5e5
name <you@example.com>
2023-03-02 15:02:29 +0300

    hw8

---
+++ homework_8/Task_54/Program.cs
@@ -0,0 +1,67 @@
+﻿int m = GetAndPrintValue("Введите количество строк: ");
+int n = GetAndPrintValue("Введите количество столбцов: ");
+int [,] array = new int[m, n];
+
+int GetAndPrintValue(string msg)
+{
+    Console.Write(msg);
+    string input = Console.ReadLine();
+    int value = Convert.ToInt32(input);
+    return value;
+}
+
+int[,] FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+
+        }
+    }
+    return array;
+}
+
+
+
+int[,] SortArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+  return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("Отсортированный массив: ");
+SortArray(array);
+PrintArray(array);
+
+
+    
+
---
+++ homework_8/Task_62/Program.cs
@@ -0,0 +1,37 @@
+﻿int n = 4;
+int[,] sqareArray = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqareArray.GetLength(0) * sqareArray.GetLength(1))
+{
+  sqareArray[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < sqareArray.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= sqareArray.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > sqareArray.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+PrintArray(sqareArray);
+
+void PrintArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}

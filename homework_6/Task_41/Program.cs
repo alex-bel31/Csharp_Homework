e07fbbf47c663db8a60e28d268da1e7faeff877b
name <you@example.com>
2023-03-02 15:02:05 +0300

    hw6

---
+++ homework_6/Task_41/Program.cs
@@ -0,0 +1,27 @@
+﻿string GetAndPrintValue(string msg)
+{
+    Console.Write(msg);
+    string input = Console.ReadLine();
+    return input;
+}
+
+string array = GetAndPrintValue("Введите массив: ");
+string[] array1 = array.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[] array2 = new int[array1.Length];
+
+for (int i = 0; i < array2.Length; i++)
+{
+    array2[i] = int.Parse(array1[i]);
+}
+
+int count = 0;
+for (int i = 0; i < array2.Length; i++)
+{
+    if (array2[i] > 0)
+    {
+        count++;
+        
+    }
+}
+
+Console.Write($" -> {count}");
---
+++ homework_6/Task_43/Program.cs
@@ -0,0 +1,16 @@
+﻿double b1 = GetAndPrintValue("Введите значение b1");
+double k1 = GetAndPrintValue("Введите число k1"); 
+double b2 = GetAndPrintValue("Введите значение b2");
+double k2 = GetAndPrintValue("Введите число k2");
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2; 
+Console.WriteLine($"b1 = {b1}, k1 {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+
+double GetAndPrintValue(string msg)
+{
+    Console.WriteLine(msg);
+    string input = Console.ReadLine();
+    double value = Convert.ToDouble(input);
+    return value;
+}
